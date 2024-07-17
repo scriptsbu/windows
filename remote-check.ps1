c3dcd0217e0bdc8eb4d33ddc1b105bf2192f59c8
Alberto Lopez-Santiago <137096347+scriptsbu@users.noreply.github.com>
2024-07-17 18:21:22 -0500

    Update remote-check.ps1

--- remote-check.ps1
+++ remote-check.ps1
@@ -1,3 +1,9 @@
+ # Get device information
+$computerName = $env:COMPUTERNAME
+$userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$serialNumber = (Get-CimInstance -ClassName Win32_BIOS).SerialNumber
+$hostName = [System.Net.Dns]::GetHostName()
+
 # Check if CrowdStrike agent is installed
 $crowdStrikePackage = Get-Package -Name "*CrowdStrike*" -ErrorAction SilentlyContinue
 $crowdStrikeInstalled = $null -ne $crowdStrikePackage
@@ -9,20 +15,49 @@ $globalProtectInstalled = $null -ne $globalProtectPackage
 $globalProtectVersion = if ($globalProtectInstalled) { $globalProtectPackage.Version } else { "Not installed" }
 
 # Output summary
+Write-Output ""
 Write-Output "Device Summary:"
+Write-Output ""
+Write-Output "- Computer Name: $computerName"
+Write-Output "- User Name: $userName"
+Write-Output "- Serial Number: $serialNumber"
+Write-Output "- Host Name: $hostName"
+Write-Output ""
 Write-Output "- CrowdStrike agent: $crowdStrikeInstalled, Version: $crowdStrikeVersion"
 Write-Output "- GlobalProtect agent: $globalProtectInstalled, Version: $globalProtectVersion"
 
+# Check GlobalProtect portal login event
+$logFile = "C:\Program Files\Palo Alto Networks\GlobalProtect\pan_gp_event.log"
+$searchString = "Portal login completed with address"
+$prependText = "- GlobalProtect Portal login completed with:"
 
-if (Test-Path "HKLM:\SOFTWARE\Microsoft\Enrollments") {
-Write-Output "This device is enrolled in Microsoft Intune."
-} else {
-Write-Output "This device is not enrolled in Microsoft Intune."
+$result = Select-String -Path $logFile -Pattern $searchString -Context 0,0 | Select-Object -First 1 | ForEach-Object {
+    "$prependText "+ $_.Line.Trim().Substring($_.Line.IndexOf($searchString) + $searchString.Length)
 }
-$BitlockerStatus = Get-BitLockerVolume
-if ($BitlockerStatus.VolumeStatus -eq "FullyEncrypted") {
-Write-Output "Bitlocker is enabled on this device."
+
+if ($result) {
+    Write-Output $result
 } else {
-Write-Output "Bitlocker is not enabled on this device."
+    Write-Output "- GlobalProtect portal login event not found in the log file."
 }
-PAUSE 
+
+# Check if device is enrolled in Microsoft Intune
+$enrollmentStatus = if (Test-Path "HKLM:\SOFTWARE\Microsoft\Enrollments") {
+    "This device is enrolled in Microsoft Intune."
+} else {
+    "This device is not enrolled in Microsoft Intune."
+}
+Write-Output "- MDM: $enrollmentStatus"
+
+# Check Bitlocker status
+$bitlockerStatus = Get-BitLockerVolume
+$encryptionStatus = if ($bitlockerStatus.VolumeStatus -eq "FullyEncrypted") {
+    "Bitlocker is enabled on this device."
+} else {
+    "Bitlocker is not enabled on this device."
+}
+Write-Output "- Encryption: $encryptionStatus"
+
+# Pause the script for user input
+Write-Output ""
+Pause 
