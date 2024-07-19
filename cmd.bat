6df3dd963e165fbeca8aef1ae0cd0c3ae4482f03
Alberto Lopez-Santiago <137096347+scriptsbu@users.noreply.github.com>
2024-07-19 12:42:04 -0500

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,1 +1,1 @@
-runas /user:torcadmin "Powershell.exe /k Invoke-RestMethod -Uri https://github.com/scriptsbu/windows/raw/main/cs.ps1 | Invoke-Expression"
+runas /user:admin "Powershell.exe /k Invoke-RestMethod -Uri https://github.com/scriptsbu/windows/raw/main/cs.ps1 | Invoke-Expression"
