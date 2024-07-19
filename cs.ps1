e0fe1ba8d595ce7fd89d2c455e610821f3e697ac
Alberto Lopez-Santiago <137096347+scriptsbu@users.noreply.github.com>
2024-07-19 09:38:25 -0500

    Create cs.ps1

---
+++ cs.ps1
@@ -0,0 +1,17 @@
+$directoryPath = "C:\Windows\System32\drivers\Crowdstrike"
+$searchPattern = "*C-000000291*"
+
+$fileToDelete = Get-ChildItem -Path $directoryPath -Filter $searchPattern -ErrorAction SilentlyContinue
+
+if ($fileToDelete) {
+    try {
+        # Use elevated permissions to delete the file
+        Start-Process powershell.exe -Verb RunAs -ArgumentList "Remove-Item -Path `"$($fileToDelete.FullName)`" -Force"
+        Write-Output "File '$($fileToDelete.Name)' has been deleted."
+    }
+    catch {
+        Write-Output "An error occurred while trying to delete the file: $($_.Exception.Message)"
+    }
+} else {
+    Write-Output "No file containing 'C-000000291' was found in the directory '$directoryPath'."
+}
